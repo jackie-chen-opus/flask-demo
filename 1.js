6c0927778166bafd7485ffaf8263ce79116ccc36
jackie-chen-opus <jackie.chen@opus.pro>
2025-03-12 19:25:04 +0800

    add db-schema-update workflow

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
 // webhook bot --> card (bot message will not trigger a webhook to your python serer)
-  // user  action on card <-- get the messageid
+// user  action on card <-- get the messageid
 
 // find the message by messageId
- // --> get the message content (      content: '{"text":"1111"}', )
-const data = {
+// --> get the message content (      content: '{"text":"1111"}', )
+const messageData = {
   schema: "2.0",
   header: {
     event_id: "7ceca3fab4848ba5ad1ef78fe0f98bec",
@@ -15,7 +15,7 @@ const data = {
   },
   event: {
     message: {
-        // group id alt + ?
+      // group id alt + ?
       chat_id: "oc_ac5e1cea84ace380ef470bc8d37bbc81", // 会话ID
       chat_type: "group",
       content: '{"text":"1111"}',
@@ -35,3 +35,76 @@ const data = {
     },
   },
 };
+
+const emojiActionData = {
+  schema: "22.0",
+  header: {
+    event_id: "01e3546aed7325434d6e63994fd5de72",
+    token: "YJ90o3tWUQK45YB8lIKzvemTyt6AEmUx",
+    create_time: "1737455924344",
+    event_type: "im.message.reaction.created_v1",
+    tenant_key: "14ead984d00ed75a",
+    app_id: "cli_a705e8ccd8789009",
+  },
+  event: {
+    action_time: "1737455924344",
+    // message from webhook bot (datadog)
+    message_id: "om_6a9691865d2f4efee4aafd7f37c37bca",
+    operator_type: "user",
+    reaction_type: { emoji_type: "OK" },
+    user_id: {
+      open_id: "ou_0a9e07d881f28149897be52261eb1261",
+      union_id: "on_3f4147dc004d7252225d17bcd98b8659",
+      user_id: "2c1a5f3g",
+    },
+  },
+};
+
+// 1. get the message content
+const messageId = "om_6a9691865d2f4efee4aafd7f37c37bca";
+lark.message.get(messageId).then((res) => {
+  const cardJson = JSON.parse(res.content);
+
+  lark.message.reply(messageId, {
+    msg_type: "interactive",
+    card: {
+      config: {
+        wide_screen_mode: true,
+      },
+      header: {
+        title: {
+          tag: "plain_text",
+          content: "title",
+        },
+      },
+      elements: [
+        {
+          tag: "div",
+          text: {
+            tag: "lark_md",
+            content: "content",
+          },
+        },
+        {
+          tag: "action",
+          actions: [
+            {
+              tag: "button",
+              text: {
+                tag: "plain_text",
+                content: "button",
+              },
+              type: "default",
+              value: {
+                key: "value",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
+});
+
+
+{'schema': '2.0', 'header': {'event_id': '8779399c268fa3ba8651dafc05d74382', 'token': 'YJ90o3tWUQK45YB8lIKzvemTyt6AEmUx', 'create_time': '1737461730661', 'event_type': 'im.message.reaction.created_v1', 'tenant_key': '14ead984d00ed75a', 'app_id': 'cli_a705e8ccd8789009'}, 'event': {'action_time': '1737461730661', 'message_id': 'om_a64c461faa45dbdd39071d5c371caa43', 'operator_type': 'user', 'reaction_type': {'emoji_type': 'Soccer'}, 'user_id': {'open_id': 'ou_0a9e07d881f28149897be52261eb1261', 'union_id': 'on_3f4147dc004d7252225d17bcd98b8659', 'user_id': '2c1a5f3g'}}}
--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import logging
+import re
+import json
 
 app = Flask(__name__)
 
-# 设置日志级别为DEBUG
 logging.basicConfig(level=logging.DEBUG)
+
+# 处理GET请求
+@app.route('/', methods=['GET'])
+def handle_get():
+    return Response("<h3>It is OK!</h3>", mimetype='text/html')
 
 # 处理POST请求
 @app.route('/', methods=['POST'])
 def handle_post():
     # 获取POST请求的json数据
     data = request.get_json()
-    
-    # 打印接收到的数据
     app.logger.debug("Received data: %s", data)
     
     # 检查数据是否为空
@@ -26,14 +30,52 @@ def handle_post():
         # 返回 challenge 字段的值
         return {"challenge": challenge}, 200
 
-    # return "No challenge field in data", 400
-    return "No challenge field in data", 200
+    event_type = data.get('header').get('event_type')
+    # im.message.reaction.created_v1
+    # im.message.receive_v1
+    app.logger.debug(event_type)
+
+    if event_type == 'im.message.receive_v1':
+        handle_ack_command(data)
+        return "Event Recieved", 200
+    
+    '''
+    if event_type == 'im.reaction.added_v1':
+        app.logger.debug("Received im.message.receive_v1")
+        return "Event Recieved", 200
+    '''
+    return "Event Recieved", 200
 
 
-# 处理GET请求
-@app.route('/', methods=['GET'])
-def handle_get():
-    return "This is a demo", 200
+def handle_ack_command(data):
+    # 获取消息内容
+    content = data.get('event').get('message').get('content')
+    msg = json.loads(content).get('text')
+    if msg:
+        if msg.startswith('/ack'):
+            app.logger.debug("This is a ack command, command content: %s", msg)
+            # 检查消息是否匹配 /ack number
+            match = re.match(r'^/ack (\d+)$', msg)
+            if match:
+                monitor_id = match.group(1)
+                app.logger.debug(f"monitor_id: {monitor_id}")
+            else:
+                app.logger.debug("Invalid /ack command format")
+        else:
+            app.logger.debug("Not a  ack command, message content: %s", msg)
+
+
+def handle_reaction(data):
+    """
+    emoji_type: 'Soccer' 
+    """
+    emoji_type = data.get('event').get('reaction_type').get('emoji_type')
+    message_id = data.get('event').get('message_id')
+    if emoji_type == 'Soccer':
+        app.logger.debug("Received im.message.receive_v1")
+
+        return "Event Recieved", 200
+
 
 
 if __name__ == '__main__':
